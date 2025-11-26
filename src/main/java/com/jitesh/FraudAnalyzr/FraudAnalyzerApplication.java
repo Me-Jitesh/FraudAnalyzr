9f589c3cc3ebb0675abfe844f4cd25b46db98617
Jitesh Singh <jiteshs101@gmail.com>
2025-11-26 20:08:02 +0530

    Initial Structure

---
+++ src/main/java/com/jitesh/FraudAnalyzr/FraudAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.jitesh.FraudAnalyzr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jitesh/FraudAnalyzr/FraudAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jitesh.FraudAnalyzr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
