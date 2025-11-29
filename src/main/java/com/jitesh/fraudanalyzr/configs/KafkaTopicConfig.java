df7de0e6bf08c54d0735c444a45124067471d6ca
Jitesh Singh <jiteshs101@gmail.com>
2025-11-29 20:22:34 +0530

    Topic Creation Completed

--- src/main/java/com/jitesh/fraudanalyzr/configs/KafkaTopicConfig.java
+++ src/main/java/com/jitesh/fraudanalyzr/configs/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.jitesh.fraudanalyzr.configs;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -8,12 +9,20 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
 
+    @Value("${app.topics.transactions}")
+    private String transactions;
+
+    @Value("${app.topics.fraud-alerts}")
+    private String fraudAlerts;
+
     @Bean
-    public NewTopic transactionTopic() {
-        return TopicBuilder.name("transactions")
-                .partitions(1)
-                .replicas(1)
-                .build();
+    public NewTopic transactionsTopic() {
+        return TopicBuilder.name(transactions).partitions(6).replicas(1).build();
+    }
+
+    @Bean
+    public NewTopic fraudAlertsTopic() {
+        return TopicBuilder.name(fraudAlerts).partitions(3).replicas(1).build();
     }
 
 }
