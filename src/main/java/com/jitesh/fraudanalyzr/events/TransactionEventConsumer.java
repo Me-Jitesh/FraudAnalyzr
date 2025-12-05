26444d0a22c8dc8f744a9dcae478a825300392cd
Jitesh Singh <jiteshs101@gmail.com>
2025-12-05 22:44:19 +0530

    Getting More Deep Info About Kafka Header in Consumer

--- src/main/java/com/jitesh/fraudanalyzr/events/TransactionEventConsumer.java
+++ src/main/java/com/jitesh/fraudanalyzr/events/TransactionEventConsumer.java
@@ -3,6 +3,9 @@ package com.jitesh.fraudanalyzr.events;
 import com.jitesh.fraudanalyzr.models.Transaction;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -10,7 +13,8 @@ import org.springframework.stereotype.Service;
 public class TransactionEventConsumer {
 
     @KafkaListener(topics = "${app.topics.transactions}", groupId = "${app.consumer.txn.group}")
-    public void transactionConsumer(Transaction txEvents) {
+    public void transactionConsumer(Transaction txEvents, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) String offset) {
         log.info("💰 TRANSACTION RECEIVED ::  {} ", txEvents);
+        log.debug("ℹ TNX Received From Topic :: {}  at Offset :: {} ", topic, offset);
     }
 }
