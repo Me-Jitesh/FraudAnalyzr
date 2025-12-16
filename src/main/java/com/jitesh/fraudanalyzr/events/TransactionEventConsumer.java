46d42e4483f599007964ee8898423cb12d63dbca
Jitesh Singh <jiteshs101@gmail.com>
2025-12-16 10:42:33 +0530

    Multi Consumer With Single Group

--- src/main/java/com/jitesh/fraudanalyzr/events/TransactionEventConsumer.java
+++ src/main/java/com/jitesh/fraudanalyzr/events/TransactionEventConsumer.java
@@ -11,13 +11,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class TransactionEventConsumer {
 
-    @KafkaListener(topics = "${app.topics.transactions}", groupId = "${app.consumer.txn.group}")
+    @KafkaListener(topics = "${app.topics.transactions}", groupId = "${app.consumer.txn.group}", properties = {"max.poll.records=1"})
     public void transactionConsumer1(Transaction txEvents, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) String offset) {
         log.info("💰 TRANSACTION RECEIVED IN CONSUMER 1 ::  {} ", txEvents);
         log.debug("ℹ TNX RECEIVED FROM TOPIC IN CONSUMER 1 :: {}  AT OFFSET :: {} ", topic, offset);
     }
 
-    @KafkaListener(topics = "${app.topics.transactions}", groupId = "${app.consumer.txn.group}")
+    @KafkaListener(topics = "${app.topics.transactions}", groupId = "${app.consumer.txn.group}", properties = {"max.poll.records=1"})
     public void transactionConsumer2(Transaction txEvents, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) String offset) {
         log.info("💰 TRANSACTION RECEIVED IN CONSUMER 2 ::  {} ", txEvents);
         log.debug("ℹ TNX RECEIVED FROM TOPIC IN CONSUMER 2 :: {}  AT OFFSET :: {} ", topic, offset);
