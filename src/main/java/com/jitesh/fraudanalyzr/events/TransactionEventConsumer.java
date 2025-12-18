fefea1c62da39fea518b4ef94e07e4223bbaf796
Jitesh Singh <jiteshs101@gmail.com>
2025-12-18 21:28:53 +0530

    Consume Messages From Specific Partition Only

--- src/main/java/com/jitesh/fraudanalyzr/events/TransactionEventConsumer.java
+++ src/main/java/com/jitesh/fraudanalyzr/events/TransactionEventConsumer.java
@@ -3,6 +3,7 @@ package com.jitesh.fraudanalyzr.events;
 import com.jitesh.fraudanalyzr.models.Transaction;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,11 @@ public class TransactionEventConsumer {
     public void transactionConsumer2(Transaction txEvents, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) String offset) {
         log.info("💰 TRANSACTION RECEIVED IN CONSUMER 2 ::  {} ", txEvents);
         log.debug("ℹ TXN RECEIVED FROM TOPIC  :: {} IN CONSUMER 2 AT OFFSET :: {} ", topic, offset);
+    }
+
+    @KafkaListener(topics = "${app.topics.transactions}", groupId = "${app.consumer.txn.group}", topicPartitions = {@TopicPartition(topic = "${app.topics.transactions}", partitions = {"6"})})
+    public void transactionConsumer3(Transaction txEvents, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header(KafkaHeaders.OFFSET) String offset) {
+        log.info("💰 TRANSACTION RECEIVED IN CONSUMER 3 FROM SPECIFIC PARTITION 6 ::  {} ", txEvents);
+        log.debug("ℹ TXN RECEIVED FROM TOPIC  :: {} IN CONSUMER 3 AT OFFSET :: {} ", topic, offset);
     }
 }
