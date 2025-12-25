2f08e402a54fd0e4e420ac1916690d0ab84586d4
Jitesh Singh <jiteshs101@gmail.com>
2025-12-25 19:14:43 +0530

    Random Bulk Transaction Generator

--- src/main/java/com/jitesh/fraudanalyzr/controllers/TransactionController.java
+++ src/main/java/com/jitesh/fraudanalyzr/controllers/TransactionController.java
@@ -1,14 +1,13 @@
 package com.jitesh.fraudanalyzr.controllers;
 
+import com.jitesh.fraudanalyzr.models.Item;
 import com.jitesh.fraudanalyzr.models.Transaction;
 import com.jitesh.fraudanalyzr.events.TransactionEventProducer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1/txn")
@@ -35,20 +34,48 @@ public class TransactionController {
 
     @PostMapping("/pay/bulk")
     public ResponseEntity<?> bulkPayment() {
+
+        Random random = new Random();
+
+        List<String> accounts = Arrays.asList("SBI005", "PNB006", "BOB007", "HDFC008", "BOI009", "CBI010");
+        List<String> locations = Arrays.asList("India", "US", "UK", "Israel", "France", "Russia");
+        List<String> merchants = Arrays.asList("Flipkart", "Amazon", "Myntra", "Meesho", "Nykaa", "Snitch");
+        List<String> payModes = Arrays.asList("Debit", "Credit", "UPI", "Wallet");
+
         try {
             for (int i = 0; i < 10; i++) {
+
                 String txnId = "TXN-" + System.currentTimeMillis() + i;
-                String accountId = "ACC-" + new Random().nextInt(100, 10000) + i;
-                double amt = Math.round(new Random().nextDouble(1, 500000));
-                String vendor = "FLIPKART- " + UUID.randomUUID()
-                        .toString()
-                        .substring(0, 5);
+                String accountId = accounts.get(random.nextInt(accounts.size()));
+                double amt = Math.round(random.nextDouble(1, 500000));
+                String loc = locations.get(random.nextInt(locations.size()));
+                String vendor = merchants.get(random.nextInt(merchants.size()));
+                String type = payModes.get(random.nextInt(payModes.size()));
+                List<Item> items = List.of(
+                        new Item("Itm-" + random.nextInt(1000),
+                                "Product-" + random.nextInt(1000),
+                                (double) Math.round(random.nextDouble(10, 500)),
+                                random.nextInt(1, 10)),
+
+                        new Item("Itm-" + random.nextInt(1000),
+                                "Product-" + random.nextInt(1000),
+                                (double) Math.round(random.nextDouble(10, 500)),
+                                random.nextInt(1, 10)),
+
+                        new Item("Itm-" + random.nextInt(1000),
+                                "Product-" + random.nextInt(1000),
+                                (double) Math.round(random.nextDouble(10, 500)),
+                                random.nextInt(1, 10))
+                );
 
                 Transaction payment = Transaction.builder()
                         .transactionId(txnId)
                         .accountId(accountId)
                         .amount(amt)
+                        .location(loc)
                         .merchant(vendor)
+                        .type(type)
+                        .items(items)
                         .timestamp(new Date())
                         .build();
 
@@ -66,20 +93,46 @@ public class TransactionController {
 
     @PostMapping("/pay/bulk/{num}")
     public ResponseEntity<?> bulkPaymentNTimes(@PathVariable int num) {
+        Random random = new Random();
+
+        List<String> accounts = Arrays.asList("SBI005", "PNB006", "BOB007", "HDFC008", "BOI009", "CBI010");
+        List<String> locations = Arrays.asList("India", "US", "UK", "Israel", "France", "Russia");
+        List<String> merchants = Arrays.asList("Flipkart", "Amazon", "Myntra", "Meesho", "Nykaa", "Snitch");
+        List<String> payModes = Arrays.asList("Debit", "Credit", "UPI", "Wallet");
         try {
             for (int i = 0; i < num; i++) {
+
                 String txnId = "TXN-" + System.currentTimeMillis() + i;
-                String accountId = "ACC-" + new Random().nextInt(100, 10000) + i;
-                double amt = Math.round(new Random().nextDouble(1, 500000));
-                String vendor = "PAYTM- " + UUID.randomUUID()
-                        .toString()
-                        .substring(0, 5);
+                String accountId = accounts.get(random.nextInt(accounts.size()));
+                double amt = Math.round(random.nextDouble(1, 500000));
+                String loc = locations.get(random.nextInt(locations.size()));
+                String vendor = merchants.get(random.nextInt(merchants.size()));
+                String type = payModes.get(random.nextInt(payModes.size()));
+                List<Item> items = List.of(
+                        new Item("Itm-" + random.nextInt(1000),
+                                "Product-" + random.nextInt(1000),
+                                (double) Math.round(random.nextDouble(10, 500)),
+                                random.nextInt(1, 10)),
+
+                        new Item("Itm-" + random.nextInt(1000),
+                                "Product-" + random.nextInt(1000),
+                                (double) Math.round(random.nextDouble(10, 500)),
+                                random.nextInt(1, 10)),
+
+                        new Item("Itm-" + random.nextInt(1000),
+                                "Product-" + random.nextInt(1000),
+                                (double) Math.round(random.nextDouble(10, 500)),
+                                random.nextInt(1, 10))
+                );
 
                 Transaction payment = Transaction.builder()
                         .transactionId(txnId)
                         .accountId(accountId)
                         .amount(amt)
+                        .location(loc)
                         .merchant(vendor)
+                        .type(type)
+                        .items(items)
                         .timestamp(new Date())
                         .build();
 
